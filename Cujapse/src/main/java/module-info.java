9397a7b1a78fbfa63e29b7c3cb261fc1347220a7
alisonH17 <alisonhguerra@gmail.com>
2025-11-13 23:32:45 -0500

    pantalla del tutorial creada

--- Cujapse/src/main/java/module-info.java
+++ Cujapse/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.cujapse {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.desktop;
 
     opens com.example.cujapse to javafx.fxml;
     exports com.example.cujapse;
