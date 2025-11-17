c66e05772fdde9f4a1c8412812d4a2f7891b9472
Ryan2550 <rynegrete09@gmail.com>
2025-11-17 01:01:38 -0500

    Update FileWriters.java

--- Cujapse/src/main/java/logic/auxiliars/Ficheros/FileWriters.java
+++ Cujapse/src/main/java/logic/auxiliars/Ficheros/FileWriters.java
@@ -5,14 +5,14 @@ import logic.clases.Dialogue;
 import logic.clases.GameCharacter;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Random;
+
 public class FileWriters {
+
     private File fichero;
 
     public FileWriters(String nombreArchivo) {
@@ -27,59 +27,62 @@ public class FileWriters {
     }
 
     public boolean saveCharacter(GameCharacter character) throws IOException {
-        int numeroId = -1;
-        RandomAccessFile raf = new RandomAccessFile(fichero,"rw");
+        if (character == null) {
+            throw new IllegalArgumentException("Personaje nulo");
+        }
+
+        int numeroId = Integer.parseInt(character.getId());
         boolean ok = false;
-        if (character != null) {
-            Object id = character.getId();
-            if (id instanceof String) {
-                numeroId = Integer.parseInt((String) id);
-            }else if (id instanceof Integer) {
-                numeroId = (Integer) id;
-            }
+
+        try (RandomAccessFile raf = new RandomAccessFile(fichero, "rw")) {
+
             raf.seek(raf.length());
+
             raf.writeInt(numeroId);
+
             byte[] bytesCharacter = Convert.toBytes(character);
             raf.writeInt(bytesCharacter.length);
             raf.write(bytesCharacter);
+
             ok = true;
         }
-        raf.close();
+
         return ok;
     }
 
+
     public boolean saveDialogue(Dialogue dialogue, String direccionFile) throws IOException {
-        int numeroId = -1;
-        boolean ok = false;
-        try {
-            Path path = Paths.get(direccionFile);
 
-            if(!Files.exists(path)) {
-                Files.createFile(path);
-            }
-                RandomAccessFile raf = new RandomAccessFile(direccionFile,"rw");
-                raf.seek(raf.length());
-                if(dialogue != null) {
-                    Object id = dialogue.getId();
-                    if (id instanceof String) {
-                        numeroId =  Integer.parseInt((String) id);
-                    }else if (id instanceof Integer) {
-                        numeroId = (Integer) id;
-                    }
-                    raf.writeInt(numeroId);
-                    byte[] bytesDialogue = Convert.toBytes(dialogue);
-                    raf.writeInt(bytesDialogue.length);
-                    raf.write(bytesDialogue);
-                    ok = true;
-                }else{
-                    throw new IllegalArgumentException("Dialogo nulo");
-                }
-                raf.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (dialogue == null) {
+            throw new IllegalArgumentException("Dialogo nulo");
         }
-       return ok;
+
+        int numeroId = Integer.parseInt(dialogue.getId());
+        boolean ok = false;
+
+        Path path = Paths.get(direccionFile);
+
+        if (path.getParent() != null && !Files.exists(path.getParent())) {
+            Files.createDirectories(path.getParent());
+        }
+
+        if (!Files.exists(path)) {
+            Files.createFile(path);
+        }
+
+        try (RandomAccessFile raf = new RandomAccessFile(direccionFile, "rw")) {
+
+            raf.seek(raf.length());
+
+            raf.writeInt(numeroId);
+
+            byte[] bytesDialogue = Convert.toBytes(dialogue);
+            raf.writeInt(bytesDialogue.length);
+            raf.write(bytesDialogue);
+
+            ok = true;
+        }
+
+        return ok;
     }
-
-
 }
