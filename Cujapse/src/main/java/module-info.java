d1713239f055557565bb849eb96cc698b96d22e6
alisonH17 <alisonhguerra@gmail.com>
2026-01-05 00:32:24 -0500

    pantalla menu inicio OK

--- Cujapse/src/main/java/module-info.java
+++ Cujapse/src/main/java/module-info.java
@@ -4,6 +4,8 @@ module com.example.cujapse {
     requires javafx.graphics;
     requires java.desktop;
 
+    exports com.example.cujapse;
+
     // Exportamos controladores si otra parte del programa los necesita
     exports interfaz.controllers;
     exports main;
